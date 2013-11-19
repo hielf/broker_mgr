e813af500a1db949c0fd8f8bdb1585969ec32a75
hielf <wercub@tom.com>
2013-11-19 22:10:49 +0800

    commit

--- db/migrate/20130820062124_add_user_id_to_workflowexes.rb
+++ db/migrate/20130820062124_add_user_id_to_workflowexes.rb
@@ -1,5 +1,5 @@
 class AddUserIdToWorkflowexes < ActiveRecord::Migration
   def change
-    add_column :workflowexes, :user_id, :integer
+    # add_column :workflowexes, :user_id, :integer
   end
 end
--- db/migrate/20130820062140_add_user_id_to_workflowhis.rb
+++ db/migrate/20130820062140_add_user_id_to_workflowhis.rb
@@ -1,5 +1,5 @@
 class AddUserIdToWorkflowhis < ActiveRecord::Migration
   def change
-    add_column :workflowhis, :user_id, :integer
+    # add_column :workflowhis, :user_id, :integer
   end
 end
