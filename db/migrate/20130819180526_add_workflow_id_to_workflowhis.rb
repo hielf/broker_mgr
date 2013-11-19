8b2f93b791ca0bb0bbc079bd5fa0239c9ea866e5
hielf <wercub@tom.com>
2013-11-19 22:07:27 +0800

    commit

--- db/migrate/20130819180526_add_workflow_id_to_workflowhis.rb
+++ db/migrate/20130819180526_add_workflow_id_to_workflowhis.rb
@@ -1,6 +1,6 @@
 class AddWorkflowIdToWorkflowhis < ActiveRecord::Migration
   def change
-    remove_column :workflowhis, :flowid
-    add_column :workflowhis, :workflow_id, :integer
+    # remove_column :workflowhis, :flowid
+    # add_column :workflowhis, :workflow_id, :integer
   end
 end
